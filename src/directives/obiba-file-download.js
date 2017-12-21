b593e60718d39bd7f1085b7ccc502869c8c30531
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2017-12-21 15:10:58 -0500

    MK-1741 - added form params.

--- src/directives/obiba-file-download.js
+++ src/directives/obiba-file-download.js
@@ -14,6 +14,8 @@
 
   /**
    * Directive primarily aimed to download a file via a POST but the submit method can be configured.
+   * A URL with query params will be parsed and its params are added to the form params. Form params can be passed
+   * as scope params to the directive as a JSON object.
    */
   angular.module('ngObiba')
     .directive('obibaFileDownload', function () {
@@ -22,17 +24,60 @@
         replace: true,
         scope: {
           url: '<',
+          formParams: '<',
           method: '@',
           encoding: '<'
         },
         link: function (scope, element) {
 
+          function moveUrlParamsToFormParams() {
+            var url = scope.url;
+
+            if (url) {
+              var index = url.indexOf('?');
+
+              if (index > -1) {
+                url.substring(index+1).split('&').forEach(function(param) {
+                  var parts = param.split('=');
+                  if (parts && !param.hasOwnProperty(parts[0])) {
+                    scope.formParams[parts[0]] = parts[1];
+                  }
+                });
+
+                scope.url = url.substring(0, index);
+              }
+            }
+          }
+
           function onClick(/*event*/) {
+            scope.formParams = scope.formParams || {};
+            moveUrlParamsToFormParams();
+
             var form = document.createElement('form');
             form.className = 'hidden';
             form.method = scope.method || 'POST';
             form.action = scope.url;
-            form.encType = scope.encoding || 'text/csv';
+            form.accept = scope.encoding || 'text/csv';
+
+            Object.keys(scope.formParams).forEach(function(key){
+              var input = document.createElement('input');
+              var value = scope.formParams[key];
+
+              if (Array.isArray(value)) {
+                value.forEach(function(item){
+                  var arrayInput = document.createElement('input');
+                  arrayInput.name = key;
+                  arrayInput.value = item;
+                  form.appendChild(arrayInput);
+                });
+              } else {
+                input.name = key;
+                input.value = value;
+                form.appendChild(input);
+              }
+
+            });
+
             document.body.appendChild(form);
             form.submit();
             form.remove();

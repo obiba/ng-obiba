1e90b196ad90136698f7210b8fee2069393b10ca
meek0 <cedricgfont@gmail.com>
2018-09-18 15:24:41 -0400

    utils must be first

--- dist/ng-obiba.js
+++ dist/ng-obiba.js
@@ -4,262 +4,6 @@ var obiba = angular.module("ngObiba", [ "obiba.form", "obiba.notification", "obi
 
 "use strict";
 
-angular.module("obiba.graphics", [ "nvd3", "obiba.utils" ]).factory("D3GeoConfig", [ function() {
-    function D3GeoConfig() {
-        this.data = [];
-        this.color = [ "#2077b2" ];
-        this._dimensions = {
-            width: 960,
-            height: 500
-        };
-        this._scale = 150;
-        this.title = "";
-    }
-    D3GeoConfig.prototype.withData = function(data) {
-        this.data = data;
-        return this;
-    };
-    D3GeoConfig.prototype.withColor = function(color) {
-        this.color = color;
-        return this;
-    };
-    D3GeoConfig.prototype.withTitle = function(title) {
-        this.title = title;
-        return this;
-    };
-    D3GeoConfig.prototype.getDimensions = function() {
-        return this._dimensions;
-    };
-    D3GeoConfig.prototype.getScale = function() {
-        return this._scale;
-    };
-    return D3GeoConfig;
-} ]).factory("D3ChartConfig", [ function() {
-    function D3ChartConfig(aggregation) {
-        function elementClick(o) {
-            var link = o.data.link;
-            if (link && window) {
-                window.location = link;
-            }
-        }
-        var options = {
-            chart: {
-                margin: {
-                    left: 100
-                },
-                x: function(d) {
-                    return d.title;
-                },
-                y: function(d) {
-                    return d.value;
-                },
-                xAxis: {
-                    tickFormat: function(d) {
-                        return d;
-                    }
-                },
-                yAxis: {
-                    tickFormat: function(d) {
-                        return d3.format(",.0f")(d);
-                    }
-                },
-                showLabels: false,
-                showLegend: true,
-                duration: 0,
-                labelThreshold: .01,
-                groupSpacing: .5,
-                height: 250,
-                reduceXTicks: false,
-                stacked: true,
-                showControls: false,
-                tooltip: {
-                    contentGenerator: function(o) {
-                        var series = o.series[0];
-                        if (series === null) {
-                            return;
-                        }
-                        var s = "", bottom = "<span>" + series.key + ": <strong>" + series.value + "</strong></span>";
-                        if (o.value) {
-                            s = "<strong>" + o.value + "</strong><br/>";
-                        }
-                        return '<div class="chart-tooltip">' + s + bottom + "</div>";
-                    }
-                },
-                multibar: {
-                    dispatch: {
-                        elementClick: elementClick
-                    }
-                },
-                pie: {
-                    dispatch: {
-                        elementClick: elementClick
-                    }
-                }
-            },
-            title: {},
-            subtitle: {}
-        };
-        switch (aggregation) {
-          case "model-methods-design":
-          case "populations-dataCollectionEvents-model-bioSamples":
-            options.chart.type = "multiBarHorizontalChart";
-            break;
-
-          case "model-numberOfParticipants-participant-range":
-            options.chart.type = "pieChart";
-            break;
-
-          default:
-            options.chart.type = "multiBarChart";
-            break;
-        }
-        if (options.chart.type === "pieChart") {
-            options.chart.showLabels = true;
-            options.chart.labelThreshold = .1;
-            options.chart.labelSunbeamLayout = false;
-            options.chart.labelType = function(d) {
-                var percent = (d.endAngle - d.startAngle) / (2 * Math.PI);
-                return d3.format(".2%")(percent);
-            };
-        }
-        this.data = [];
-        this.options = options;
-    }
-    D3ChartConfig.prototype.withType = function(type) {
-        this.options.chart.type = type;
-        return this;
-    };
-    D3ChartConfig.prototype.withTitle = function(title) {
-        this.options.title = {
-            text: title,
-            enable: true
-        };
-        return this;
-    };
-    D3ChartConfig.prototype.withSubtitle = function(subtitle) {
-        this.options.subtitle = {
-            text: subtitle,
-            enable: true
-        };
-        return this;
-    };
-    D3ChartConfig.prototype.withData = function(data, override, key) {
-        if (override) {
-            this.data = data;
-        } else {
-            this.data.push({
-                key: key,
-                values: data
-            });
-        }
-        return this;
-    };
-    return D3ChartConfig;
-} ]).directive("obibaNvChart", [ function() {
-    function link() {}
-    return {
-        restrict: "EA",
-        template: '<nvd3 options="chartConfig.options" data="chartConfig.data"></nvd3>',
-        scope: {
-            chartConfig: "="
-        },
-        link: link
-    };
-} ]).directive("obibaGeo", [ "ObibaCountriesGeoJson", function(ObibaCountriesGeoJson) {
-    function luma(rgbColor) {
-        return rgbColor.r * .2126 + rgbColor.g * .7152 + rgbColor.b * .0722;
-    }
-    function colorComparator(colorA, colorB) {
-        if (colorA === colorB) {
-            return 0;
-        }
-        return luma(d3.rgb(colorA)) > luma(d3.rgb(colorB)) ? 1 : -1;
-    }
-    function lightestColor(colorArray) {
-        if (!Array.isArray(colorArray)) {
-            return colorArray;
-        }
-        colorArray.sort(colorComparator);
-        return colorArray.reduce(function(acc, val) {
-            if (!acc) {
-                return val;
-            }
-            var rgbVal = d3.rgb(val), rbgAcc = d3.rgb(acc);
-            return luma(rgbVal) > luma(rbgAcc) ? val : acc;
-        });
-    }
-    function ColorSelector(values, palette) {
-        var sortedUniqueValues = values.filter(function(item, pos) {
-            return values.indexOf(item) === pos;
-        }).sort(function(a, b) {
-            return a - b;
-        });
-        var configColor = lightestColor(palette);
-        this.color = function(value) {
-            var index = sortedUniqueValues.indexOf(value);
-            return index === 0 ? d3.rgb(configColor) : d3.rgb(configColor).darker(index * .005 * value);
-        };
-    }
-    function link(scope, element) {
-        var values = [];
-        var data = {};
-        function getRadio() {
-            var parentElement = element.parent()[0];
-            return parentElement.clientWidth / scope.config.getDimensions().width;
-        }
-        scope.config.data.forEach(function(d) {
-            data[d.key] = d.value;
-            values.push(d.value);
-        });
-        var max = 0;
-        for (var k in data) {
-            if (data.hasOwnProperty(k)) {
-                max = data[k] >= max ? data[k] : max;
-            }
-        }
-        function renderMap(ratio) {
-            element[0].innerHTML = "";
-            var colorSelector = new ColorSelector(values, scope.config.color);
-            var title = scope.config.title;
-            if (title) {
-                d3.select(element[0]).append("div").attr("class", "title h4").style("text-align", "center").text(title);
-            }
-            var tooltip = d3.select(element[0]).append("div").attr("class", "hidden chart-tooltip");
-            var projection = d3.geo.equirectangular().translate([ scope.config.getDimensions().width * ratio / 2, scope.config.getDimensions().height * ratio / 2 ]).scale(scope.config.getScale() * ratio);
-            var path = d3.geo.path().projection(projection);
-            var svg = d3.select(element[0]).append("svg").attr("width", scope.config.getDimensions().width * ratio).attr("height", scope.config.getDimensions().height * ratio);
-            var g = svg.append("g");
-            g.selectAll("path").data(ObibaCountriesGeoJson.features).enter().append("path").attr("d", path).style("fill", function(d) {
-                return data[d.id] ? colorSelector.color(data[d.id]) : d3.rgb("#ccc");
-            }).style("stroke", "#fff").on("mousemove", function(d) {
-                var mouse = d3.mouse(svg.node()).map(function(d) {
-                    return parseInt(d);
-                });
-                var tooltipText = d.properties.name;
-                if (typeof data[d.id] !== "undefined") {
-                    tooltipText = tooltipText + " <strong>" + data[d.id] + "</strong>";
-                }
-                tooltip.classed("hidden", false).attr("style", "left:" + (mouse[0] + 5) + "px; top:" + (mouse[1] - 5) + "px; position: absolute").html(tooltipText);
-            }).on("mouseout", function() {
-                tooltip.classed("hidden", true);
-            });
-        }
-        renderMap(getRadio());
-        d3.select(window).on("resize", function() {
-            renderMap(getRadio());
-        });
-    }
-    return {
-        restrict: "EA",
-        scope: {
-            config: "="
-        },
-        link: link
-    };
-} ]);
-
-"use strict";
-
 obiba.utils = angular.module("obiba.utils", []);
 
 obiba.utils.NgObibaStringUtils = function() {
@@ -2227,6 +1971,262 @@ obiba.utils.EventListenerRegistry = function() {
     this.unregister = unregister;
     this.unregisterAll = unregisterAll;
 };
+
+"use strict";
+
+angular.module("obiba.graphics", [ "nvd3", "obiba.utils" ]).factory("D3GeoConfig", [ function() {
+    function D3GeoConfig() {
+        this.data = [];
+        this.color = [ "#2077b2" ];
+        this._dimensions = {
+            width: 960,
+            height: 500
+        };
+        this._scale = 150;
+        this.title = "";
+    }
+    D3GeoConfig.prototype.withData = function(data) {
+        this.data = data;
+        return this;
+    };
+    D3GeoConfig.prototype.withColor = function(color) {
+        this.color = color;
+        return this;
+    };
+    D3GeoConfig.prototype.withTitle = function(title) {
+        this.title = title;
+        return this;
+    };
+    D3GeoConfig.prototype.getDimensions = function() {
+        return this._dimensions;
+    };
+    D3GeoConfig.prototype.getScale = function() {
+        return this._scale;
+    };
+    return D3GeoConfig;
+} ]).factory("D3ChartConfig", [ function() {
+    function D3ChartConfig(aggregation) {
+        function elementClick(o) {
+            var link = o.data.link;
+            if (link && window) {
+                window.location = link;
+            }
+        }
+        var options = {
+            chart: {
+                margin: {
+                    left: 100
+                },
+                x: function(d) {
+                    return d.title;
+                },
+                y: function(d) {
+                    return d.value;
+                },
+                xAxis: {
+                    tickFormat: function(d) {
+                        return d;
+                    }
+                },
+                yAxis: {
+                    tickFormat: function(d) {
+                        return d3.format(",.0f")(d);
+                    }
+                },
+                showLabels: false,
+                showLegend: true,
+                duration: 0,
+                labelThreshold: .01,
+                groupSpacing: .5,
+                height: 250,
+                reduceXTicks: false,
+                stacked: true,
+                showControls: false,
+                tooltip: {
+                    contentGenerator: function(o) {
+                        var series = o.series[0];
+                        if (series === null) {
+                            return;
+                        }
+                        var s = "", bottom = "<span>" + series.key + ": <strong>" + series.value + "</strong></span>";
+                        if (o.value) {
+                            s = "<strong>" + o.value + "</strong><br/>";
+                        }
+                        return '<div class="chart-tooltip">' + s + bottom + "</div>";
+                    }
+                },
+                multibar: {
+                    dispatch: {
+                        elementClick: elementClick
+                    }
+                },
+                pie: {
+                    dispatch: {
+                        elementClick: elementClick
+                    }
+                }
+            },
+            title: {},
+            subtitle: {}
+        };
+        switch (aggregation) {
+          case "model-methods-design":
+          case "populations-dataCollectionEvents-model-bioSamples":
+            options.chart.type = "multiBarHorizontalChart";
+            break;
+
+          case "model-numberOfParticipants-participant-range":
+            options.chart.type = "pieChart";
+            break;
+
+          default:
+            options.chart.type = "multiBarChart";
+            break;
+        }
+        if (options.chart.type === "pieChart") {
+            options.chart.showLabels = true;
+            options.chart.labelThreshold = .1;
+            options.chart.labelSunbeamLayout = false;
+            options.chart.labelType = function(d) {
+                var percent = (d.endAngle - d.startAngle) / (2 * Math.PI);
+                return d3.format(".2%")(percent);
+            };
+        }
+        this.data = [];
+        this.options = options;
+    }
+    D3ChartConfig.prototype.withType = function(type) {
+        this.options.chart.type = type;
+        return this;
+    };
+    D3ChartConfig.prototype.withTitle = function(title) {
+        this.options.title = {
+            text: title,
+            enable: true
+        };
+        return this;
+    };
+    D3ChartConfig.prototype.withSubtitle = function(subtitle) {
+        this.options.subtitle = {
+            text: subtitle,
+            enable: true
+        };
+        return this;
+    };
+    D3ChartConfig.prototype.withData = function(data, override, key) {
+        if (override) {
+            this.data = data;
+        } else {
+            this.data.push({
+                key: key,
+                values: data
+            });
+        }
+        return this;
+    };
+    return D3ChartConfig;
+} ]).directive("obibaNvChart", [ function() {
+    function link() {}
+    return {
+        restrict: "EA",
+        template: '<nvd3 options="chartConfig.options" data="chartConfig.data"></nvd3>',
+        scope: {
+            chartConfig: "="
+        },
+        link: link
+    };
+} ]).directive("obibaGeo", [ "ObibaCountriesGeoJson", function(ObibaCountriesGeoJson) {
+    function luma(rgbColor) {
+        return rgbColor.r * .2126 + rgbColor.g * .7152 + rgbColor.b * .0722;
+    }
+    function colorComparator(colorA, colorB) {
+        if (colorA === colorB) {
+            return 0;
+        }
+        return luma(d3.rgb(colorA)) > luma(d3.rgb(colorB)) ? 1 : -1;
+    }
+    function lightestColor(colorArray) {
+        if (!Array.isArray(colorArray)) {
+            return colorArray;
+        }
+        colorArray.sort(colorComparator);
+        return colorArray.reduce(function(acc, val) {
+            if (!acc) {
+                return val;
+            }
+            var rgbVal = d3.rgb(val), rbgAcc = d3.rgb(acc);
+            return luma(rgbVal) > luma(rbgAcc) ? val : acc;
+        });
+    }
+    function ColorSelector(values, palette) {
+        var sortedUniqueValues = values.filter(function(item, pos) {
+            return values.indexOf(item) === pos;
+        }).sort(function(a, b) {
+            return a - b;
+        });
+        var configColor = lightestColor(palette);
+        this.color = function(value) {
+            var index = sortedUniqueValues.indexOf(value);
+            return index === 0 ? d3.rgb(configColor) : d3.rgb(configColor).darker(index * .005 * value);
+        };
+    }
+    function link(scope, element) {
+        var values = [];
+        var data = {};
+        function getRadio() {
+            var parentElement = element.parent()[0];
+            return parentElement.clientWidth / scope.config.getDimensions().width;
+        }
+        scope.config.data.forEach(function(d) {
+            data[d.key] = d.value;
+            values.push(d.value);
+        });
+        var max = 0;
+        for (var k in data) {
+            if (data.hasOwnProperty(k)) {
+                max = data[k] >= max ? data[k] : max;
+            }
+        }
+        function renderMap(ratio) {
+            element[0].innerHTML = "";
+            var colorSelector = new ColorSelector(values, scope.config.color);
+            var title = scope.config.title;
+            if (title) {
+                d3.select(element[0]).append("div").attr("class", "title h4").style("text-align", "center").text(title);
+            }
+            var tooltip = d3.select(element[0]).append("div").attr("class", "hidden chart-tooltip");
+            var projection = d3.geo.equirectangular().translate([ scope.config.getDimensions().width * ratio / 2, scope.config.getDimensions().height * ratio / 2 ]).scale(scope.config.getScale() * ratio);
+            var path = d3.geo.path().projection(projection);
+            var svg = d3.select(element[0]).append("svg").attr("width", scope.config.getDimensions().width * ratio).attr("height", scope.config.getDimensions().height * ratio);
+            var g = svg.append("g");
+            g.selectAll("path").data(ObibaCountriesGeoJson.features).enter().append("path").attr("d", path).style("fill", function(d) {
+                return data[d.id] ? colorSelector.color(data[d.id]) : d3.rgb("#ccc");
+            }).style("stroke", "#fff").on("mousemove", function(d) {
+                var mouse = d3.mouse(svg.node()).map(function(d) {
+                    return parseInt(d);
+                });
+                var tooltipText = d.properties.name;
+                if (typeof data[d.id] !== "undefined") {
+                    tooltipText = tooltipText + " <strong>" + data[d.id] + "</strong>";
+                }
+                tooltip.classed("hidden", false).attr("style", "left:" + (mouse[0] + 5) + "px; top:" + (mouse[1] - 5) + "px; position: absolute").html(tooltipText);
+            }).on("mouseout", function() {
+                tooltip.classed("hidden", true);
+            });
+        }
+        renderMap(getRadio());
+        d3.select(window).on("resize", function() {
+            renderMap(getRadio());
+        });
+    }
+    return {
+        restrict: "EA",
+        scope: {
+            config: "="
+        },
+        link: link
+    };
+} ]);
 
 "use strict";
 

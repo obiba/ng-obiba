923e4d8b84f120a2273cb4143d6e9848890ecd1d
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2020-09-01 16:55:48 -0400

    Updated dist/

--- dist/ng-obiba.js
+++ dist/ng-obiba.js
@@ -3180,6 +3180,11 @@ angular.module("obiba.simpleMde", [ "templates-main" ]);
                     action: SimpleMDE.togglePreview,
                     className: "fa fa-eye no-disable",
                     title: LocaleStringUtils.translate("simple-mde.toolbar.preview")
+                }, {
+                    name: "fullScreen",
+                    action: SimpleMDE.toggleFullScreen,
+                    className: "fa fa-arrows-alt no-disable",
+                    title: LocaleStringUtils.translate("simple-mde.toolbar.fullScreen")
                 } ];
                 function onSimpleMdeChange() {
                     scope.text = simpleMde.value();
